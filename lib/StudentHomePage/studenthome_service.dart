6bd071751959bee3baea7bb3d0032f92e5a4d37d
Bedoui Wassim <ow_bedoui@esi.dz>
2026-03-16 12:07:13 +0100

    Add backend to the student page

---
+++ lib/StudentHomePage/studenthome_service.dart
@@ -0,0 +1,45 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fahamni/models/session_model.dart';
+import 'package:fahamni/models/tutor_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../models/student_model.dart';
+
+class studenthomepage_service {
+  final FirebaseFirestore _db = FirebaseFirestore.instance;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Get current student data
+  Future<StudentModel> getStudentData() async {
+    final uid ="8Gy2hIY4DUWBwlgOaZs0yEaGdt73";  //_auth.currentUser!.uid;
+
+    final doc = await _db.collection('students').doc(uid).get();
+    try {
+      return StudentModel.fromMap(doc.data()!);
+    } catch (e) {
+      print('ERROR: $e');
+      rethrow;
+    }
+  }
+  Future<TutorModel> getTutorData(String id) async {
+
+    final doc = await _db.collection('tutors').doc(id).get();
+    try {
+      return TutorModel.fromMap(doc.data()!);
+    } catch (e) {
+      print('ERROR: $e');
+      rethrow;
+    }
+  }
+  Future<List<TutorModel>> getFavoriteTeachers(List<String> ids) async {
+    final docs = await Future.wait(
+        ids.map((id) => _db.collection('tutors').doc(id).get())
+    );
+    return docs.map((doc) => TutorModel.fromMap(doc.data()!)).toList();
+  }
+  Future<List<SessionModel>> getCourses(List<String> ids) async {
+    final docs = await Future.wait(
+        ids.map((id) => _db.collection('sessions').doc(id).get())
+    );
+    return docs.map((doc) => SessionModel.fromMap(doc.data()!)).toList();
+  }
+}
