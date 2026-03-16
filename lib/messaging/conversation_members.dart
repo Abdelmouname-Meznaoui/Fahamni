270bd539c9945b864d009c551e850003f04cc654
Hamza-Bhm24 <oh_benrabah@esi.dz>
2026-03-17 00:36:42 +0100

    conversation members/details

---
+++ lib/messaging/conversation_members.dart
@@ -0,0 +1,77 @@
+import 'package:fahamni/messaging/chat_buttons.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'dart:ui';
+import 'package:google_fonts/google_fonts.dart';
+
+class ConversationMembers extends StatelessWidget {
+  ConversationMembers({super.key});
+
+  final List<String> conversation_members = [
+    'Mohamed djamel',
+    'Hamza Benrabah', 
+    'Mohamed djamel',
+    'Mohamed djamel',
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      itemCount: conversation_members.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                backgroundImage: NetworkImage('https://anniversaire-celebrite.com/images/celebrites/patrick-etoile-de-mer.jpg'),
+                radius: 24.0,
+              ),
+
+              const SizedBox(width: 8.0),
+
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      conversation_members[index],
+                      style: GoogleFonts.inter(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.bold,
+                        color: const Color(0xFF1F2937),
+                      ),
+                    ),
+                    Text(
+                      'Student',
+                      style: GoogleFonts.inter(
+                        fontSize: 14.0,
+                        fontWeight: FontWeight.w500,
+                        color: const Color(0xFF000080), 
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              
+              Container(
+                width: 45,
+                height: 45,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF000080),
+                  shape: BoxShape.circle,
+                ),
+                child: const Icon(
+                  Icons.chat_bubble_outline, 
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
